0bf82fdc81fb6ebf94d5e41b0aec7b4bc94891f4
katzemf <118005961+katzemf@users.noreply.github.com>
2022-11-16 20:43:10 +0100

    details_barchart0.2

--- services/frontend/src/components/ConfigurationPanel.vue
+++ services/frontend/src/components/ConfigurationPanel.vue
@@ -6,19 +6,6 @@
   <div>
     <v-container fluid>
       <v-row>
-        <!--<v-col cols="12" md="12">
-          <v-card>
-            Parameters go here
-          </v-card>
-        </v-col>-->
-
-        <!--<v-col cols="12" md="8">
-          <v-card>
-            Overview
-          </v-card>
-          <OverView/>
-        </v-col>-->
-
         <v-col cols="12" md="8" style=" border-collapse :collapse;border-width:0px">
           <v-card-title cols="12" md="8">
             <v-spacer />
@@ -29,39 +16,29 @@
             <v-spacer />
           </v-card-title>
           <v-card-actions cols="12" md="8">
-            <!--<v-spacer />
-            <v-btn color="lightblue">Depressed</v-btn>
-            <v-spacer />
-            <v-spacer />
-            <v-btn color="lightblue">Non-Depressed</v-btn>
-            <v-spacer />-->
-            <!--<select>
-              <option value ="details" selected="selected">Please select here</option>
-              <option value ="details">Details</option>
-              <option value ="average">Average</option>
-            </select>-->
-
             <select name="Select" class="selected">
               <option value="details" selected="selected" data-skip="1">Please select here</option>
-              <option value="details" data-icon="img/products/iphone.png" data-html-text="details">Overview</option>
-              <option value="average" data-icon="img/products/ipod.png" data-html-text="average">Average</option>
+              <option value="details" data-icon="xx.png" data-html-text="details">Overview</option>
+              <option value="average" data-icon="xx.png" data-html-text="average">Average</option>
               </select>
-
           </v-card-actions>
           <OverView/>
         </v-col>
-
-
 
         <v-col cols="12" md="4" style=" border-collapse :collapse;border-width:0px">
           <v-card-title cols="12" md="4">
             <v-spacer />
             <div class="text-center">
-              <h3 class="headline black--text text--accent-2">Details</h3>
+              <h3 class="headline black--text text--accent-2">Detail Exploration</h3>
               <!--<div>Some description about the headline</div>-->
             </div>
             <v-spacer />
           </v-card-title>
+          <select name="Select" class="selected">
+            <option value="Depressed" selected="selected" data-skip="1">Please select here</option>
+            <option value="Depressed" data-icon="xx.png" data-html-text="details">Depressed</option>
+            <option value="Non-Depressed" data-icon="xx.png" data-html-text="average">Non-Depressed</option>
+          </select>
           <DetailsAlign/>
         </v-col>
 
