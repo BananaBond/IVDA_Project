c817ad47cf4b998109a25d6ce8041011b8ee4113
tanshiv2 <shivangi.tanwar4@gmail.com>
2022-11-18 14:59:50 +0100

    overview graph with average and boxplot

--- services/frontend/src/components/BoxPlot.vue
+++ services/frontend/src/components/BoxPlot.vue
@@ -7,6 +7,9 @@
 
 <script>
 import Plotly from 'plotly.js/dist/plotly';
+// import depressed from '/Users/shivangi/Downloads/generated_dataset/depressed.json';
+// import times from '/Users/shivangi/Downloads/generated_dataset/time.json';
+
 export default {
   name: "BoxPlot",
   data: () => ({
@@ -115,15 +118,43 @@ export default {
               name: time,
               marker: {
                   color: 'rgb(107,174,214)'
-              }
+              },
+              showlegend:false
           }
           data.push(new_trace)
           new_trace = {}
       })
 
+      // var y_list = []
+
+      // for(var ff=0; ff<1440; ff++){
+      //   if (ff%60 == 0){}
+      //     y_list.push(depressed["files"][1]["activity_level"][ff])
+      // }
+
+      // var bp_times = ['00:00:00','01:00:00','02:00:00','03:00:00','04:00:00','05:00:00','06:00:00','07:00:00','08:00:00'
+      //     ,'09:00:00','10:00:00','11:00:00','12:00:00','13:00:00','14:00:00','15:00:00','16:00:00','17:00:00','18:00:00'
+      //     ,'19:00:00','20:00:00','21:00:00','22:00:00','23:00:00']
+      // bp_times.forEach(time => {
+      //     var new_trace = {
+      //         y: y_list,
+      //         type: 'box',
+      //         name: time,
+      //         marker: {
+      //             color: 'rgb(107,174,214)'
+      //         }
+      //     }
+      //     data.push(new_trace)
+      //     new_trace = {}
+      // })
+      
+
       // Plotly.newPlot('myScatterPlot', data);
       // var config = {responsive: true, displayModeBar: false}
       var layout = {
+        autosize: false,
+        width: 1000,
+        height: 500,
           title: {
           text:'condition_19',
           font: {
